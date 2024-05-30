a88328925068d290e23d5ea6a063b98fe8d88591
Asyra20 <rasyraf7@gmail.com>
2024-05-30 22:51:44 +0700

    tambahan font

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jbag/src/features/admin/admin_homescreen.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -6,7 +7,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: ,
+      home: HomeS(
+        
+      ),
     );
   }
 }
---
+++ lib/src/features/admin/admin_homescreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeS extends StatelessWidget {
+  const HomeS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder(
+      
+    );
+  }
+}
