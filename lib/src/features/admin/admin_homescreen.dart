e78640ac6a5b1bf7859bf1f1fcb8657cf2689b47
Asyra20 <rasyraf7@gmail.com>
2024-06-02 00:12:44 +0700

    admin hal 1 (belum jadi)

--- lib/src/features/admin/admin_homescreen.dart
+++ lib/src/features/admin/admin_homescreen.dart
@@ -5,8 +5,91 @@ class HomeS extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFF131A2A),
+      body: AdminHomescreen(),
+    );
+  }
+}
+
+class AdminHomescreen extends StatefulWidget {
+  const AdminHomescreen({super.key});
+
+  @override
+  State<AdminHomescreen> createState() => _AdminHomescreenState();
+}
+
+class _AdminHomescreenState extends State<AdminHomescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF131A2A),
+        body: Center(
+          child: Container(
+            color: Color(0xFF131A2A),
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'ADMIN',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  ),
+                  child: Text(
+                    'REVIEW AKUN PENJUAL',
+                    style: TextStyle(
+                      color: Color(0xFF131A2A),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  ),
+                  child: Text(
+                    'CEK TRANSAKSI',
+                    style: TextStyle(
+                      color: Color(0xFF131A2A),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  ),
+                  child: Text(
+                    'LOGOUT',
+                    style: TextStyle(
+                      color: Color(0xFF131A2A),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
