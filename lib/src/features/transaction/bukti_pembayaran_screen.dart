7b6e7b9d270655f2f96697dab184cc978f37f49f
z4fL <zaaaafl654@gmail.com>
2024-06-02 22:51:53 +0700

    proses transaksi pembeli UI checkout, transaksi, bukti #7

--- lib/src/features/transaction/bukti_pembayaran_screen.dart
+++ lib/src/features/transaction/bukti_pembayaran_screen.dart
@@ -1,10 +1,243 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class BuktiPembayaranScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+
+class BuktiPembayaranScreen extends StatefulWidget {
   const BuktiPembayaranScreen({super.key});
 
   @override
+  State<BuktiPembayaranScreen> createState() => _BuktiPembayaranScreenState();
+}
+
+class _BuktiPembayaranScreenState extends State<BuktiPembayaranScreen> {
+  File? _image;
+
+  Future getImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => _image = imageTemp);
+    } on PlatformException catch (e) {
+      print('failed to pick image: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFF131A2A),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF131A2A),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const FaIcon(
+            FontAwesomeIcons.chevronLeft,
+            color: Color(0xFFFFFAFF),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Bukti Pembayaran',
+                    style: TextStyle(
+                      fontFamily: 'BebasNeue',
+                      fontSize: 48,
+                      color: Color(0xFFFFFAFF),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 450,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFECE8E1),
+                      ),
+                      child: _image == null
+                          ? Center(
+                              child: IconButton(
+                                onPressed: () {
+                                  openModalOption(context);
+                                },
+                                icon: const FaIcon(
+                                  FontAwesomeIcons.fileImport,
+                                  size: 80,
+                                ),
+                              ),
+                            )
+                          : Image.file(
+                              _image!,
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center,
+                              filterQuality: FilterQuality.medium,
+                            ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: MyButton(
+                text: "Batal",
+                color: const Color(0xFFF9564F),
+                onPressed: () => setState(() {
+                  _image = null;
+                }),
+              ),
+            ),
+            const SizedBox(width: 30),
+            Expanded(
+              child: MyButton(
+                text: "Upload",
+                color: const Color(0xFFFFC639),
+                onPressed: () {
+                  BuildContext dialogContext;
+                  showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      dialogContext = context;
+                      return Dialog(
+                        backgroundColor: const Color(0xFFECE8E1),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 16),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "Pemberitahuan",
+                                style: TextStyle(
+                                    fontFamily: 'BebasNeue', fontSize: 38),
+                              ),
+                              const SizedBox(height: 10),
+                              const Text(
+                                "Pembelian Akun akan Segera Diproses oleh Penjual",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontFamily: 'BebasNeue', fontSize: 28),
+                              ),
+                              const SizedBox(height: 25),
+                              MyButton(
+                                text: "Ke Halaman Riwayat",
+                                onPressed: () {
+                                  Navigator.of(
+                                    dialogContext,
+                                    rootNavigator: true,
+                                  ).pop();
+                                },
+                                color: const Color(0xFFFFC639),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void openModalOption(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              tileColor: const Color(0xFFECE8E1),
+              leading: const FaIcon(FontAwesomeIcons.camera),
+              title: const Text('Camera'),
+              onTap: () {
+                getImage(ImageSource.camera);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              tileColor: const Color(0xFFECE8E1),
+              leading: const FaIcon(FontAwesomeIcons.solidImage),
+              title: const Text('Gallery'),
+              onTap: () {
+                getImage(ImageSource.gallery);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.color,
+    this.padding = 20,
+  });
+
+  final String text;
+  final Color color;
+  final VoidCallback onPressed;
+  final double padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        foregroundColor: const Color(0xFF131A2A),
+        padding: EdgeInsets.symmetric(horizontal: padding),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+        textStyle: const TextStyle(
+          fontSize: 32,
+          fontFamily: 'BebasNeue',
+          color: Color(0xFF393E46),
+        ),
+      ),
+      child: Text(text),
+    );
   }
 }
--- lib/src/features/transaction/transaksi_screen.dart
+++ lib/src/features/transaction/transaksi_screen.dart
@@ -1,10 +1,259 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:jbag/src/features/transaction/bukti_pembayaran_screen.dart';
+import 'package:jbag/src/utils/format/currency_format.dart';
 
 class TransaksiScreen extends StatelessWidget {
-  const TransaksiScreen({super.key});
+  const TransaksiScreen({
+    super.key,
+    required this.transaksiData,
+  });
+
+  final Map<String, dynamic> transaksiData;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFF131A2A),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF131A2A),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const FaIcon(
+            FontAwesomeIcons.chevronLeft,
+            color: Color(0xFFFFFAFF),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text(
+                'Transaksi',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'BebasNeue',
+                  fontSize: 48,
+                  color: Color(0xFFFFFAFF),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    transaksiData['judul'],
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontFamily: 'LeagueGothic',
+                      fontSize: 38,
+                      color: Color(0xFFFFFAFF),
+                    ),
+                  ),
+                  Text(
+                    transaksiData['penjual'],
+                    style: const TextStyle(
+                      fontFamily: 'LeagueGothic',
+                      fontSize: 24,
+                      color: Color(0xFFECE8E1),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: const Color(0xFFFFFAFF),
+                      child: Image.asset(
+                        transaksiData['payment']['paymentMethod']['image'],
+                        height: 40,
+                        alignment: Alignment.centerLeft,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: const Color(0xFFECE8E1),
+                      child: Text(
+                        transaksiData["payment"]["namaProfilEwallet"],
+                        style: const TextStyle(
+                          color: Color(0xFF393E46),
+                          fontSize: 32,
+                          fontFamily: 'LeagueGothic',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: const Color(0xFFECE8E1),
+                      child: Text(
+                        transaksiData["payment"]["nomorEwallet"],
+                        style: const TextStyle(
+                          color: Color(0xFF393E46),
+                          fontSize: 32,
+                          fontFamily: 'LeagueGothic',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFFFFC639), width: 2),
+                ),
+                child: Column(
+                  children: [
+                    HargaRow(
+                      text: "Harga Total",
+                      harga: transaksiData["hargaTotal"].toInt(),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 8.0),
+                child: Text(
+                  "*Pastikan tidak salah input saat melakukan pembayaran",
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 12,
+                    color: Color(0xFFFFFAFF),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: MyButton(
+                text: "Batal",
+                color: const Color(0xFFF9564F),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+            const SizedBox(width: 30),
+            Expanded(
+              child: MyButton(
+                text: "Selanjutnya",
+                color: const Color(0xFFFFC639),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return BuktiPembayaranScreen();
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.color,
+    this.padding = 20,
+  });
+
+  final String text;
+  final Color color;
+  final VoidCallback onPressed;
+  final double padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        foregroundColor: const Color(0xFF131A2A),
+        padding: EdgeInsets.symmetric(horizontal: padding),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+        textStyle: const TextStyle(
+          fontSize: 32,
+          fontFamily: 'BebasNeue',
+          color: Color(0xFF393E46),
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
+
+class HargaRow extends StatelessWidget {
+  const HargaRow({
+    super.key,
+    required this.harga,
+    required this.text,
+  });
+
+  final String text;
+  final int harga;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          text,
+          style: const TextStyle(
+            fontFamily: 'LeagueGothic',
+            fontSize: 28,
+            color: Color(0xFFECE8E1),
+          ),
+        ),
+        Text(
+          CurrencyFormat.convert2Idr(harga, 2),
+          style: const TextStyle(
+            fontFamily: 'BebasNeue',
+            fontSize: 32,
+            color: Color(0xFFECE8E1),
+          ),
+        ),
+      ],
+    );
   }
 }
