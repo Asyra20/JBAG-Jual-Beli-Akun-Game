5e61357018b840e9a5d67b348615390ff26c7f59
sriPujangga <pujanggaa436@gmail.com>
2024-06-09 23:29:48 +0700

    membuat class untuk variable  constant Fixes #1

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const primary = Color(0xFFECE8E1);
+  static const secondary = Color(0xFF393E46);
+  static const tertiary = Color(0xFFF9564F);
+  static const accent = Color(0xFFFFC639);
+  static const white = Color(0xFFFFFAFF);
+  static const dark = Color(0xFF131A2A);
+}
