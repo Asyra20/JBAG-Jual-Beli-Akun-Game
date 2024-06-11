1d5fd3ba2c56c0f562649b237529771c2857b78c
Asyra20 <rasyraf7@gmail.com>
2024-06-11 21:47:55 +0700

    daftar akun penjual

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jbag/src/features/account_games/daftar_akun_penjual.dart';
 import 'package:jbag/src/features/auth/login_screen.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:LoginScreen(),
+      home: MyHomePage(),
     );
   }
 }
